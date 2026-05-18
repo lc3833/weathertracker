37ce41a27bdbb3253c0dbc2683f47871cc357fff
lc3833 <lc20253833@student.fon.bg.ac.rs>
2026-05-19 01:31:35 +0200

    Initialize Spring Boot project, pom.xml and database configuration

---
+++ src/main/java/com/projekat/weathertracker/WeathertrackerApplication.java
@@ -0,0 +1,13 @@
+package com.projekat.weathertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeathertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeathertrackerApplication.class, args);
+	}
+
+}
